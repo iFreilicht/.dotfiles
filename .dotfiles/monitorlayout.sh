d6d6b6cfaecfbf3f5599112b2ec9925cebc7a69d
Felix Uhl <felix.uhl@outlook.com>
2020-04-04 16:36:22 +0200

    Improved multi-screen behaviour of polybar

--- .dotfiles/monitorlayout.sh
+++ .dotfiles/monitorlayout.sh
@@ -2,7 +2,7 @@
 case $(hostname) in
     iFreilicht-LZ7)
         # Desktop, two screens
-        xrandr --output DP-1 --auto --output DVI-I-1  --auto --left-of DP-1xrandr --output DP-1 --auto --output DVI-I-1 --auto --left-of DP-1
+        xrandr --output DP-1 --primary --auto --output DVI-I-1  --auto --left-of DP-1
     ;;
     felix-XPS-12-Ubuntu)
         echo "On Laptop"
