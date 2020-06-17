9e9d9004fd992648da6e70cdfa0dc62d957edc9a
Felix Uhl <felix.uhl@outlook.com>
2020-06-17 19:00:14 +0200

    Simplify installation script

--- .dotfiles/install_essentials.sh
+++ .dotfiles/install_essentials.sh
@@ -1,33 +1,25 @@
-# Jump to directories
-sudo apt install -y autojump
-# Display directories as trees
-sudo apt install -y tree 
-# Show system info
-sudo apt install -y neofetch
-# rofi task switcher / launcher
-sudo apt install -y rofi
-# Set numlock via console
-sudo apt install -y numlockx
-# Comprehensive man pages
-sudo apt install -y tldr
-# Backgrounds
-sudo apt install -y nitrogen
-# Dependencies of splatmoji
-sudo apt install -y xdotool xsel
-# Playerctl, for media keys
-wget http://ftp.nl.debian.org/debian/pool/main/p/playerctl/libplayerctl2_2.0.1-1_amd64.deb \
-&& wget http://ftp.nl.debian.org/debian/pool/main/p/playerctl/playerctl_2.0.1-1_amd64.deb \
-&& sudo dpkg -i libplayerctl2_2.0.1-1_amd64.deb playerctl_2.0.1-1_amd64.deb
-# Dependencies of teiler
-sudo apt install -y xininfo ffmpeg xclip maim slop copyq
-# install teiler from git repo
-cd .dotfiles/run/teiler && sudo make install; cd
+#!/usr/bin/env bash
 
-# Install ag (mainly for fzf)
-sudo apt install -y silversearcher-ag
-
-# Install apt-installable dependencies of ranger
+# Install apt-installable stuff
 apt_deps=(
+    # Dependencies of polybar
+    cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev
+    libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev
+    libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen
+    xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev
+    libiw-dev libcurl4-openssl-dev libpulse-dev
+    libxcb-composite0-dev xcb libxcb-ewmh2
+    libplayerctl2 playerctl # Media key support
+    silversearcher-ag # ag (mainly for fzf)
+    xininfo ffmpeg xclip maim slop copyq # Dependencies of teiler
+    xdotool xsel # Dependencies of splatmoji
+    nitrogen # Backgrounds
+    tldr # Comprehensive man pages
+    numlockx # Set numlock via console
+    rofi # rofi task switcher / launcher
+    neofetch # Show system info
+    tree # Display directories as trees
+    autojump # Jump to directories
     unrar # Previewing archives
     mediainfo # View info about media files
     fontforge # Font previews
@@ -36,25 +28,18 @@ apt_deps=(
 sudo apt install -y $apt_deps
 # Install python dependencies of ranger and ranger itself
 py_deps=(
+    i3ipc # i3 wrapper for i3-alternating-layout
     chardet # Improved encoding detection for text files
     ueberzug # Very fast image previews
     ranger-fm
 )
 sudo python3 -m pip install $py_deps
 
-# Install i3 python wrapper (for i3-alternating-layout)
-sudo python3 -m pip install i3-py
-
-# Install dependencies of polybar
-sudo apt install -y \
-  cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev \
-  libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev \
-  libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen \
-  xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev \
-  libiw-dev libcurl4-openssl-dev libpulse-dev \
-  libxcb-composite0-dev xcb libxcb-ewmh2
 # install polybar
 cd .dotfiles/run/polybar && ./build.sh --all-features --auto; cd 
+
+# install teiler from git repo
+cd .dotfiles/run/teiler && sudo make install; cd
 
 # Install all the vim stuff
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
