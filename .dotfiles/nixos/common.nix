96b6a9625321661eaba1197c911b27adc7a02d6f
Felix Uhl <github@mail.felix-uhl.de>
2024-05-12 20:34:42 +0200

    Lock nixpkgs flake-ref by default

--- .dotfiles/nixos/common.nix
+++ .dotfiles/nixos/common.nix
@@ -1,5 +1,5 @@
 # Options I want to enable on every system
-{ config, pkgs, ... }:
+{ config, pkgs, nixpkgs, ... }:
 
 {
   # Select internationalisation properties.
@@ -11,7 +11,25 @@
   # };
 
   # Nix settings
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+
+    # Lock 'nixpkgs' in flake-refs to the same nixpkgs this configuration is built from
+    # This prevents downloads of newer versions of nixpkgs when using `nix shell` or `nix run`
+    # (This seems to be the default in 24.05, but not earlier versions)
+    registry = {
+      nixpkgs = {
+        from = {
+          type = "indirect";
+          id = "nixpkgs";
+        };
+        to = {
+          type = "path";
+          path = nixpkgs;
+        };
+      };
+    };
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
