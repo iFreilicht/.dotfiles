iFreilicht/.dotfiles

commit 866dcfc538468c0f0c4f81903a31924853c42e58

Access services directly at home
junction now acts as the main DNS server for my home, which means that
all services don't require internet access anymore if my internet is
down. It doesn't seem like roun-trip times improved, running

    time https 192.168.178.13/login Host:cloud.uhl.cx --verify=no

takes exactly the same amount of time, but for raw throughput this
should be vastly better. And it's more efficient for sure.

.dotfiles/nixos/junction/modules/nginx.nix
@@ -1,0 +1,47 @@
+{ config, net, ... }:
+
+{
+  # Automatically renewing SSL certificates
+  security.acme = {
+    defaults.email = "letsencrypt@mail.felix-uhl.de";
+    acceptTerms = true;
+
+    certs.${net.domain} = {
+      # Empty on purpose, each module should set its own subdomain for this
+      extraDomainNames = [ ];
+      dnsProvider = "hetzner";
+      dnsPropagationCheck = true;
+      credentialFiles = {
+        HETZNER_API_KEY_FILE = config.sops.secrets.hetzner-dns-api-key.path;
+      };
+    };
+  };
+
+  sops.secrets.hetzner-dns-api-key = {
+    reloadUnits = [ "acme-cert-${net.domain}.service" ];
+  };
+
+  services.nginx = {
+    enable = true;
+
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+
+    # Harden nginx as described in https://nixos.wiki/wiki/nginx#Hardened_setup_with_TLS_and_HSTS_preloading
+    appendHttpConfig = ''
+      # Add HSTS header with preloading to HTTPS requests.
+      # Adding this header to HTTP requests is discouraged
+      map $scheme $hsts_header {
+          https   "max-age=31536000; includeSubdomains";
+      }
+    '';
+  };
+
+  # Allow nginx to read the challenges from acme
+  users.users.nginx.extraGroups = [ "acme" ];
+
+  # Allow HTTP and HTTPS traffic to reach nginx
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+}

.dotfiles/nixos/junction/configuration.nix
@@ -9,6 +9,8 @@
       ../modules/user-felix.nix
       ./modules/hardware-configuration.nix # Include the results of the hardware scan.
       ./modules/disko.nix # Drive configuration
+      ./modules/nginx.nix
+      ./modules/dns.nix
       ./modules/nextcloud.nix
       ./modules/borg.nix
       ./modules/containerized-apps.nix

.dotfiles/nixos/junction/modules/borg.nix
@@ -28,12 +28,8 @@ in
     ${repos.databases.host}.publicKey = publicKey;
   };
 
-  sops = {
-    secrets = {
-      "borg/passphrase" = {
-        reloadUnits = [ "borgmatic.service" ];
-      };
-    };
+  sops.secrets."borg/passphrase" = {
+    reloadUnits = [ "borgmatic.service" ];
   };
 
   services.borgmatic =

.dotfiles/nixos/junction/modules/containerized-apps.nix
@@ -31,6 +31,32 @@ in
     };
   };
 
+  security.acme.certs.${net.domain} = {
+    extraDomainNames = [
+      net.snapdrop.domain
+      net.kritzeln.domain
+    ];
+  };
+
+  services.nginx.virtualHosts = {
+    ${net.snapdrop.domain} = {
+      useACMEHost = net.domain;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://localhost:${toString net.snapdrop.port}";
+        proxyWebsockets = true;
+      };
+    };
+    ${net.kritzeln.domain} = {
+      useACMEHost = net.domain;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://localhost:${toString net.kritzeln.port}";
+        proxyWebsockets = true;
+      };
+    };
+  };
+
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [
     net.snapdrop.port

.dotfiles/nixos/junction/modules/dns.nix
@@ -1,0 +1,69 @@
+{ pkgs, net, ... }:
+
+let
+  # YYYYMMDDRR where RR can be increased every time a change is made on the same day
+  # Only ever increase this number!
+  serialn = "2024080901"; # Substitution ${serialn} has same length as string itself
+  adminEmail = "admin.${net.domain}"; # Doesn't actually exist, but DNS is private, so doesn't matter
+in
+{
+  services.bind = {
+    enable = true;
+    forwarders = [
+      # The router gets its DNS from the ISP, which might publish some routes other DNS servers don't know about
+      net.home.router
+      # Fall back on Cloudflare and Google DNS servers
+      "1.1.1.1"
+      "1.0.0.1"
+      "8.8.8.8"
+      "8.8.4.4"
+    ];
+
+    # Only allow queries from my private networks
+    cacheNetworks = [
+      net.home.subnet
+      net.gateway.wireguard.subnet
+      "127.0.0.0/24"
+      "::1/128"
+    ];
+
+    # Ensure the local zone is only forwarded to the router, never the other DNS servers.
+    # this is necessary so that local hostnames resolve properly
+    extraConfig = ''
+      zone "${net.home.zone}" IN {
+        type forward;
+        forward only;
+        forwarders { ${net.home.router}; };
+      };
+    '';
+
+
+    zones = {
+      ${net.domain} = {
+        master = true;
+        file = pkgs.writeText "${net.domain}.zone" ''
+          $TTL 86400
+          @   IN  SOA ns1.${net.domain}. ${adminEmail}. (
+                ${serialn} ; Serial
+                3600       ; Refresh
+                900        ; Retry
+                1209600    ; Expire
+                86400 )    ; Minimum TTL
+              NS  ns1.${net.domain}.
+          ns1      IN A ${net.junction.home.ip}
+          @        IN A ${net.junction.home.ip}
+          cloud    IN A ${net.junction.home.ip}
+          kritzeln IN A ${net.junction.home.ip}
+          drop     IN A ${net.junction.home.ip}
+        '';
+        # TODO: generate records from configuration
+      };
+    };
+  };
+
+  # DNS uses UDP port 53 normally, but also TCP port 53 for large responses
+  networking.firewall = {
+    allowedUDPPorts = [ 53 ];
+    allowedTCPPorts = [ 53 ];
+  };
+}

.dotfiles/nixos/junction/modules/nextcloud.nix
@@ -17,9 +17,25 @@
   environment.etc."nextcloud-admin-pass".text = "default-admin-pass-plz-change";
 
   # Set port nextcloud is reachable on
-  services.nginx.virtualHosts.${net.nextcloud.domain}.listen = [
-    { addr = "0.0.0.0"; port = net.nextcloud.port; }
-  ];
+  services.nginx.virtualHosts.${net.nextcloud.domain} = {
+    listen = [
+      # Port to listen on for traffic from gateway
+      { addr = "0.0.0.0"; port = net.nextcloud.port; }
+      # Default addresses to listen on for local access
+      { addr = "0.0.0.0"; port = 80; }
+      { addr = "[::0]"; port = 80; }
+      { addr = "0.0.0.0"; port = 443; ssl = true; }
+      { addr = "[::0]"; port = 443; ssl = true; }
+    ];
+    useACMEHost = net.domain;
+    addSSL = true;
+    # TODO: enforce SSL
+  };
+
+  # Ensure our certificate also covers the nextcloud domain
+  security.acme.certs.${net.domain} = {
+    extraDomainNames = [ net.nextcloud.domain ];
+  };
 
   # Nextcloud itself
   services.nextcloud = {

.dotfiles/nixos/junction/secrets.yaml
@@ -1,5 +1,6 @@
 borg:
     passphrase: ENC[AES256_GCM,data:IKr35vnR0d18Lf3BYKVlLfPX4uvtrK/44sPuGY1c69h3CEWwG1DfD7dgQAf7aBHpZd+GBzvERA==,iv:nMqg9PyL75gmt51Z8CxS+iNVmRAYn+ciWzvq71mLEXA=,tag:pM/gp9G5bSlGN3K++WLeqQ==,type:str]
+hetzner-dns-api-key: ENC[AES256_GCM,data:r3hASh7GGTYRZ53tvhcTB1IFhGFiVyIYSN2r4W8Beow=,iv:7UqlZLVa4OXLyN9kcRZKHfVtzFKYSe6u+UfPpwD1Ia8=,tag:qjByDkkHMX9QDHM+qiWuUg==,type:str]
 sops:
     kms: []
     gcp_kms: []
@@ -24,8 +25,8 @@ sops:
             YXQvcXlIZ1Rvak9xdDRzdHc0ZlNNL00KQlVah2gzQSVHvkRWGy6w7Bz0euixKcEC
             QBEc2C40JugqQka0vmbZQQOCsejbpKfnci45iyKpskeXNID0w19yVQ==
             -----END AGE ENCRYPTED FILE-----
-    lastmodified: "2024-08-04T12:05:12Z"
-    mac: ENC[AES256_GCM,data:cJIviXMA09EllZByDNKMGc5GciLz0xUcRlJsOGwDKUr2L5mM9iFrNHhUj0q5SzX3mHD3/IWWV7F4rjNixuNcL1gpYKt7QSBEexUuq4WbAUq5OUVYfgNzJI22XbC7DZa+WXnL45iJaG+PnvDVkIlEy/sZvamtB3imSESWfGPex0s=,iv:lUvzWzKoRGaB8/+q178DeJcQtNEuslikdizsHPEWnIw=,tag:RJIwmox4SEwh6WVcEXTUXQ==,type:str]
+    lastmodified: "2024-08-09T11:39:38Z"
+    mac: ENC[AES256_GCM,data:Dm8fRIrt49nJdwL0Fmw8yNBvuZwGrz7gpZWMrH2JZvlUrrlQcEkuo3d4/INzq886/BIkbaPRGHOI4NDPXP0Yv7VRNKCVA3WXYJQDJY3XK6hkIbj48jR/n0VVRxhTtJpFQW0Ju/ia0deH/17Ukvny0TYsLxImSuz3VJYq/dOeqEU=,iv:BZNY2/JbkfYvGBUD+XliK0/Z9uj+fwZlsdzEyMTLBGY=,tag:y9n9l/kAwekWU4cYApqOQQ==,type:str]
     pgp: []
     unencrypted_suffix: _unencrypted
     version: 3.8.1

.dotfiles/nixos/network.nix
@@ -5,17 +5,29 @@ let
     path = "ssh://${id}@${id}.repo.borgbase.com/./repo";
     label = label;
   };
+  rootDomain = "uhl.cx";
+  mkService = subDomain: port: {
+    inherit subDomain port;
+    domain = "${subDomain}.${rootDomain}";
+  };
+  homeBaseIP = "192.168.178";
+  wgBaseIP = "10.100.0";
 in
 {
-  nextcloud = { domain = "cloud.uhl.cx"; port = 33001; };
-  snapdrop = { domain = "drop.uhl.cx"; port = 33002; };
-  kritzeln = { domain = "kritzeln.uhl.cx"; port = 33003; };
-  home.subnet = "192.168.178.0/24";
+  domain = rootDomain;
+  nextcloud = mkService "cloud" 33001;
+  snapdrop = mkService "drop" 33002;
+  kritzeln = mkService "kritzeln" 33003;
+  home = {
+    subnet = "${homeBaseIP}.0/24";
+    router = "${homeBaseIP}.1";
+    zone = "fritz.box";
+  };
   gateway = {
     wireguard = {
-      ip = "10.100.0.1";
+      ip = "${wgBaseIP}.1";
       # Only route traffic of the wireguard subnet through the VPN, not the whole internet
-      subnet = "10.100.0.0/24";
+      subnet = "${wgBaseIP}.0/24";
       # ON REINSTALL: Run `sudo cat /etc/wireguard/private | wg pubkey` and update this value
       publicKey = "70NDFa+EmxNDZLW3QFO3blILT3oRA5K3aIofjLPdIxg=";
       initialIP = "49.12.239.37"; # The static IP the server can be reached at
@@ -23,8 +35,10 @@ in
   };
   junction = {
     name = "junction";
+    # ON REINSTALL: Ensure the router is statically setting the IP of junction to this
+    home.ip = "${homeBaseIP}.13"; # Statically set in router
     wireguard = {
-      ip = "10.100.0.13";
+      ip = "${wgBaseIP}.13";
       # ON REINSTALL: Run `sudo cat /etc/wireguard/private | wg pubkey` and update this value
       publicKey = "ocMusNfO8N6z4kc2FEJMwhFTdRV4VWbKyAhGZMDzJSE=";
     };
@@ -40,7 +54,7 @@ in
     felix.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB9OOXhRhYgpaFLwbkfcQsSYYUTr+qsbf0WIHcUm2fFQ felix@horse";
     root.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMj4suEfNQKtFyYVlO3bgawvKuM/FWYtgu6BPMe5R8ia root@horse";
     wireguard = {
-      ip = "10.100.0.8";
+      ip = "${wgBaseIP}.8";
       publicKey = "adcMoJUfbf+RTtRt6oXCggop1XDWGfiWyGQzA9gmpB0=";
     };
   };

commit 6385c528d4b22dcc8b1a27ca44b8cb40fee432ad

Fix ACME DNS propagation check
This issue was introduced in 866dcfc because my local DNS doesn't add
new entries based on DNS propagation.
I didn't notice at the time because my existing certs were still valid.

.dotfiles/nixos/junction/modules/nginx.nix
@@ -11,6 +11,9 @@
       extraDomainNames = [ ];
       dnsProvider = "hetzner";
       dnsPropagationCheck = true;
+      # Use Cloudflare as resolver for DNS propagation check, as my local DNS will not recursively
+      # resolve entries like _acme-challenge.cloud.uhl.cx properly, making the check fail
+      dnsResolver = "1.1.1.1:53";
       credentialFiles = {
         HETZNER_API_KEY_FILE = config.sops.secrets.hetzner-dns-api-key.path;
       };