31b428362c672b79f50973f5c8855cf42669d61c
Felix Uhl <github@mail.felix-uhl.de>
2024-09-29 15:10:38 +0200

    Enable bluetooth on source

--- .dotfiles/nixos/source/modules/hardware-configuration.nix
+++ .dotfiles/nixos/source/modules/hardware-configuration.nix
@@ -76,4 +76,9 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
 }
