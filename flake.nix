5298194884410f3ef5d8fdd01cd97dc1763288c6
Felix Uhl <github@mail.felix-uhl.de>
2024-10-31 10:17:45 +0100

    Upgrade dependencies

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     # and https://hydra.nixos.org/jobset/nixpkgs/nixpkgs-23.11-darwin for stable
     # and select the latest commit hash that has no unfinished builds (meaning it's fully cached).
     # Use `git rev-parse 53a2c32` in a local nixpkgs checkout to find the full hash quickly.
-    nixpkgs.url = "github:NixOS/nixpkgs/568bfef547c14ca438c56a0bece08b8bb2b71a9c";
+    nixpkgs.url = "github:NixOS/nixpkgs/2d2a9ddbe3f2c00747398f3dc9b05f7f2ebb0f53";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/3281bec7174f679eabf584591e75979a258d8c40";
 
     flake-utils.url = "github:numtide/flake-utils";
