283acd3542e35ef7946d007840968f52ce6ef6a4
Felix Uhl <github@mail.felix-uhl.de>
2024-11-21 20:47:42 +0100

    Fix home build on darwin
    zig is broken for darwin again, so the ncdu install fails

--- home-manager/modules/packages/default.nix
+++ home-manager/modules/packages/default.nix
@@ -6,6 +6,7 @@ let
   defaultPackages =
     (with pkgs-stable; [
       tlrc # Quick command help, tldr rust client. Command is tldr, not tlrc
+      ncdu # Disk usage analyzer
     ])
     ++ (with pkgs; [
       # Basic terminal setup
@@ -37,7 +38,6 @@ let
       gnupg # PGP toolkit
       age # Encryption tool
       duf # Quick disk space view
-      ncdu # Disk usage analyzer
 
       # Programming stuff
       asdf-vm # Version manager for all sorts of tools
