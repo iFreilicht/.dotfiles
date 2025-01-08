7a5ebccb7f0af77f1143006fdc3ed26b133579b5
Felix Uhl <github@mail.felix-uhl.de>
2025-01-08 11:31:06 +0100

    Fix broken home config

--- home-manager/nixos-module.nix
+++ home-manager/nixos-module.nix
@@ -2,7 +2,10 @@
 {
   home-manager = {
     useGlobalPkgs = true;
-    useUserPackages = true;
+    # Docs say useUserPackages has to be set to true, but
+    # then some packages are only partially installed
+    useUserPackages = false; 
+
     extraSpecialArgs = {
       inherit net;
       # Would be better to actually pass in pkgs-stable, but doesn't really matter on NixOS
