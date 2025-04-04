5376ef3397dbad67a15df57aa3181847278ee087
Felix Uhl <github@mail.felix-uhl.de>
2025-04-04 20:24:55 +0200

    Upgrade Nextcloud

--- nixos/junction/modules/nextcloud.nix
+++ nixos/junction/modules/nextcloud.nix
@@ -67,7 +67,7 @@
   # Nextcloud itself
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud30;
+    package = pkgs.nextcloud31;
     hostName = net.nextcloud.domain;
     home = mnt.nextcloud;
     configureRedis = true;
