828e9ad2f17baf6aea0971657be13030d27d62f4
Felix Uhl <github@mail.felix-uhl.de>
2025-10-19 21:04:50 +0200

    Remove intel-media-sdk
    It has been marked as unsafe due to multiple CVEs, see
    https://www.intel.com/content/www/us/en/security-center/advisory/intel-sa-00935.html
    
    This will probably break hardware encoding/decoding on junction,
    as it has a 4th gen processor, but I'll have to look at that at a later point in time.

--- nixos/junction/modules/hardware-configuration.nix
+++ nixos/junction/modules/hardware-configuration.nix
@@ -48,7 +48,6 @@
       vaapiVdpau
       intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
       vpl-gpu-rt # QSV on 11th gen or newer
-      intel-media-sdk # QSV up to 11th gen
     ];
   };
 }
