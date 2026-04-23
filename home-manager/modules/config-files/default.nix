de342ca27273d00ece5f6b020a383aedf875f6e5
Felix Uhl <github@mail.felix-uhl.de>
2026-04-23 22:07:03 +0200

    Symlink files for ranger because it refuses to start otherwise

--- home-manager/modules/config-files/default.nix
+++ home-manager/modules/config-files/default.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ config, ... }:
+let
+  symlink =
+    name:
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/home-manager/modules/config-files/${name}";
+in
 {
   xdg.configFile = {
     "kitty/kitty.conf".source = ./kitty/kitty.conf;
-    "ranger".source = ./ranger;
+    "ranger".source = symlink "ranger"; # Ranger refuses to start if the config directory is not writeable
     "zellij".source = ./zellij;
     "asdf-default-npm-packages".source = ./asdf-default-npm-packages;
   };
